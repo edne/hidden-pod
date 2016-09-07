6d67d1922823d355d31f1b2174ac0453a169d136
Edoardo Negri <edne@gmx.com>
2016-09-07 10:22:35 +0200

    Serve files on hidden service

--- src/hidden_pod/core.clj
+++ src/hidden_pod/core.clj
@@ -10,27 +10,25 @@
 (defn publish-hidden-service
   "Create an hidden service forwarding a port, return the address"
   [local-port remote-port]
-  (let [tor-folder (->> (into-array java.nio.file.attribute.FileAttribute [])
-                        (Files/createTempDirectory "tor-folder")
-                        .toFile)
-        onion-proxy-content (new JavaOnionProxyContext tor-folder)
-        onion-proxy-manager (new JavaOnionProxyManager onion-proxy-content)]
-    (if (.startWithRepeat onion-proxy-manager 30 5)
-      (let [onion-url (.publishHiddenService onion-proxy-manager 80 4000)]
-        onion-url)
+  (let [onion-proxy-manager (->> (into-array java.nio.file.attribute.FileAttribute [])
+                                 (Files/createTempDirectory "tor-folder") .toFile
+                                 (new JavaOnionProxyContext)
+                                 (new JavaOnionProxyManager))]
+    (if (.startWithRepeat onion-proxy-manager 30 5)   ;; false if fails
+      (.publishHiddenService onion-proxy-manager
+                             remote-port local-port)  ;; return .onion address
       (throw (Exception. "Failed to run Tor")))))
 
 
-(defn handler [request]
+(defn file-not-found [request]
   {:status 404
    :headers {"Content-Type" "text/html"}
    :body "File not found"})
 
 
 (defn -main
-  ;"For now just forward a port to an hidden service"
-  "For now just serve a folder"
+  "Serve a folder on an hidden service"
   [& args]
-  ;(println (publish-hidden-service 3000 80))
-  (run-jetty (wrap-file handler (first args))
+  (println "Serving at:" (publish-hidden-service 3000 80))
+  (run-jetty (wrap-file file-not-found (first args))
              {:port 3000}))
