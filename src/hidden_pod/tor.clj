038ee98911e15c5e428780555af0713d40a87d81
Edoardo Negri <edne@gmx.com>
2016-09-08 12:46:54 +0200

    Move functions outside publish-hidden-service

--- src/hidden_pod/tor.clj
+++ src/hidden_pod/tor.clj
@@ -4,14 +4,23 @@
                                                     JavaOnionProxyManager)))
 
 
+(defn- start-proxy [proxy-manager]
+  (if-not (.startWithRepeat proxy-manager 30 5)
+    (throw (Exception. "Failed to run Tor"))))
+
+
+(defn- new-proxy-manager []
+  (let [proxy-manager (->> (into-array java.nio.file.attribute.FileAttribute [])
+                           (Files/createTempDirectory "tor-folder") .toFile
+                           (new JavaOnionProxyContext)
+                           (new JavaOnionProxyManager))]
+    (start-proxy proxy-manager)
+    proxy-manager))
+
+
 (defn publish-hidden-service
   "Create an hidden service forwarding a port, return the address"
   [local-port remote-port]
-  (let [onion-proxy-manager (->> (into-array java.nio.file.attribute.FileAttribute [])
-                                 (Files/createTempDirectory "tor-folder") .toFile
-                                 (new JavaOnionProxyContext)
-                                 (new JavaOnionProxyManager))]
-    (if (.startWithRepeat onion-proxy-manager 30 5)   ;; false if fails
-      (.publishHiddenService onion-proxy-manager
-                             remote-port local-port)  ;; return .onion address
-      (throw (Exception. "Failed to run Tor")))))
+  (let [onion-addr (.publishHiddenService (new-proxy-manager)
+                                          remote-port local-port)]
+    onion-addr))
